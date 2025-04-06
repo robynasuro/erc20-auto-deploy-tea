781ff6e968a7f5e5497030329c4517b74c16f24c
robynasuro <103092372+robynasuro@users.noreply.github.com>
2025-04-06 13:47:43 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,12 @@
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
-const { RPC_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    tea_sepolia: {
-      url: RPC_URL,
-      accounts: [PRIVATE_KEY],
+    custom: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
