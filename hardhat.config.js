935ec46e08c0f772a69d05282f81936515f4541d
robynasuro <103092372+robynasuro@users.noreply.github.com>
2025-04-06 15:00:25 +0700

    Update default hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "tea",
   networks: {
     tea: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      url: "https://rpc.sepolia.tea.xyz",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.20",
 };
