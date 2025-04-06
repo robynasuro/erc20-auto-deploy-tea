effa89c1210b94113bf73f7de841c2d4f6a703cb
robynasuro <103092372+robynasuro@users.noreply.github.com>
2025-04-06 13:52:31 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,12 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    custom: {
+    tea: {
       url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
